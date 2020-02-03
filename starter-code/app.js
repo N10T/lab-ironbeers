89fbf04800641838ee7baac1e00cd24741d4dd4e
N10T <andy.tisba@gmail.com>
2020-02-03 17:29:50 +0100

    iteration 3-4 | ok

--- starter-code/app.js
+++ starter-code/app.js
@@ -16,4 +16,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 // add the routes here:
 app.get('/', (req, res) => res.render('index'));
 
+app.get('/beers', (req, res) => {
+    punkAPI
+    .getBeers()
+    .then(beers => {
+        console.log('Beers from the database: ', beers)
+        res.render('beers',{beers})
+})
+    .catch(error => console.log(error));   
+});
+
+
+app.get('/random-beer', (req, res) => {
+    punkAPI
+    .getRandom()
+    .then(beer => {
+        const randomBeer = beer[0]
+        console.log('Beer from the database: ', beer[0])
+        res.render('random-beer',{randomBeer})
+    })
+    .catch(error => console.log(error))
+})
+
+
+
 app.listen(3000, () => console.log('🏃‍ on port http://localhost:3000'));
